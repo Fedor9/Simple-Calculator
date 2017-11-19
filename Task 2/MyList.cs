5c66818ba200abf468f4f783caa8feea3967f43b
Fedor9 <H4rUxPNz>
2017-11-19 17:01:02 +0500

    task 2
    init commit

---
+++ Task 2/MyList.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceToClass
+{
+    class MyList<T> : IList<T>
+    {
+        List<T> list = new List<T>();
+
+        public T this[int index] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public int Count { get; set; }
+        public bool IsReadOnly { get; set; }
+
+        public void Add(T item)
+        {
+            list.Add(item);
+        }
+
+        public void Clear()
+        {
+            list.Clear();
+        }
+
+        public bool Contains(T item)
+        {
+            return list.Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            list.CopyTo(array, arrayIndex);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return list.GetEnumerator();
+        }
+
+        public int IndexOf(T item)
+        {
+            return list.IndexOf(item);
+        }
+
+        public void Insert(int index, T item)
+        {
+            list.Insert(index,item);
+        }
+
+        public bool Remove(T item)
+        {
+            return list.Remove(item);
+        }
+
+        public void RemoveAt(int index)
+        {
+            list.RemoveAt(index);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return list.GetEnumerator();
+        }
+    }
+}
